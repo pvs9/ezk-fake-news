4233d76c8134ba7325ac35a42f836696c98de132
Gogabok <gogabok123@gmail.com>
2022-06-11 11:48:17 +0300

    Setup Home page

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,14 +1,56 @@
 <template>
-  <Searcher />
+  <Searcher id="Searcher" />
+  <ArticleDetails
+    id="ArticleDetails"
+    v-if="isAnimate || article"
+  />
 </template>
 
 <script>
-import Searcher from "../components/Template/Searcher.vue";
+// Components
+import Searcher from "../components/Template/Searcher/index.vue";
+import ArticleDetails from "../components/Template/ArticleDetails/index.vue";
+// Store
+import { useArticleStore } from "../store/Article";
+
+const animationDuration = 700;
 
 export default {
   name: "Home",
   components: {
     Searcher,
+    ArticleDetails,
+  },
+  data() {
+    return {
+      isAnimate: false,
+    };
+  },
+  computed: {
+    article() {
+      const store = useArticleStore();
+      return store.article || null;
+    },
+  },
+  watch: {
+    article: {
+      handler: function (e) {
+        if (!e) {
+          this.isAnimate = true;
+          this.$scrollTo("#Searcher", animationDuration);
+          setTimeout(() => {
+            this.isAnimate = false;
+          }, animationDuration);
+
+          return;
+        }
+
+        this.$nextTick(() => {
+          return this.$scrollTo("#ArticleDetails", animationDuration);
+        });
+      },
+      deep: true,
+    },
   },
 };
 </script>
