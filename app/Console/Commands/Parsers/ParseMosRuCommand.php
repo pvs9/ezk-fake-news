c80dad735fcd5691ac98eef68d21ee0af58706ae
Vladimir Patrikeev <vpatrikeev@likebz.ru>
2022-06-12 00:34:56 +0300

    Roadrunner prepare

---
+++ app/Console/Commands/Parsers/ParseMosRuCommand.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Console\Commands\Parsers;
+
+use App\Models\Article;
+use Carbon\CarbonImmutable;
+use Illuminate\Console\Command;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Str;
+
+class ParseMosRuCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'mos-ru:parse';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Parse https://mos.ru tech news';
+
+    public function handle()
+    {
+        $now = CarbonImmutable::now();
+        $from = $now->startOfYear()->subYears(2)->toDateString();
+        $to = $now->toDateString();
+        $page = 1;
+        $totalPages = 1;
+
+        while ($page <= $totalPages) {
+            $response = Http::get(
+                sprintf(
+                    'https://mos.ru/api/newsfeed/v4/frontend/json/ru/articles?fields=id,title,date,full_text,&filter={"spheres.id":"183299","!=id":[108162073,108148073,108124073,108157073,108150073,108128073,108160073,108138073,108123073]}&from=%s&per-page=50&sort=-date&to=%s&page=%s',
+                    $from, $to, $page
+                )
+            );
+
+            if ($response->status() !== 200) {
+                break;
+            }
+
+            $data = $response->json();
+            $totalPages = $data['_meta']['pageCount'];
+            $items = $data['items'];
+            $preparedItems = Arr::map($items, function ($item, $key) {
+                return [
+                    'external_id' => $item['id'],
+                    'source' => 'https://mos.ru',
+                    'title' => $item['title'],
+                    'text' => trim(
+                        preg_replace('!\s+!', ' ',
+                            html_entity_decode(
+                                str_replace('&nbsp;',' ', strip_tags($item['full_text']))
+                            )
+                        )
+                    ),
+                    'date' => $item['date'],
+                ];
+            });
+
+            Article::query()->upsert(
+                $preparedItems,
+                ['external_id', 'source'],
+                ['title', 'text', 'date']
+            );
+
+            $page++;
+        }
+    }
+}
---
+++ app/Models/Article.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
+
+/**
+ * App\Models\Article
+ *
+ * @property int $id
+ * @property int $external_id
+ * @property string $title
+ * @property string $text
+ * @property string|null $author
+ * @property string $source
+ * @property Carbon $date
+ * @property int $comments_count
+ * @property int $is_reliable
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @method static Builder|Article newModelQuery()
+ * @method static Builder|Article newQuery()
+ * @method static Builder|Article query()
+ * @method static Builder|Article whereAuthor($value)
+ * @method static Builder|Article whereCommentsCount($value)
+ * @method static Builder|Article whereCreatedAt($value)
+ * @method static Builder|Article whereDate($value)
+ * @method static Builder|Article whereId($value)
+ * @method static Builder|Article whereIsFake($value)
+ * @method static Builder|Article whereSource($value)
+ * @method static Builder|Article whereText($value)
+ * @method static Builder|Article whereTitle($value)
+ * @method static Builder|Article whereUpdatedAt($value)
+ */
+class Article extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'external_id',
+        'title',
+        'text',
+        'author',
+        'source',
+        'date',
+        'comments_count',
+        'is_reliable',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'date' => 'datetime',
+        'is_reliable' => 'boolean'
+    ];
+}
