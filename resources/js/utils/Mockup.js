35678b4c99073d83843dfffe9c960290d276e36f
Gogabok <gogabok123@gmail.com>
2022-06-11 11:50:36 +0300

    Add Requests mockup

---
+++ resources/js/utils/Mockup.js
@@ -0,0 +1,19 @@
+const article = {
+    authenticity: 87,
+    tonality: 52,
+    source_reliability: 23,
+    article: {
+        title: "Название статьи",
+        author: "Автор статьи"
+    },
+    pdf_report: "https://ya.ru/",
+    original_source: {
+        title: "Название оригинальной статьи",
+        date: "23 января 2021",
+        link: "https://google.com/",
+        similarity_percent: 54,
+        tonality: 55
+    },
+}
+
+export { article };
