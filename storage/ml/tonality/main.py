fa3becf54e88f79962df9bc90b8627c2ab9d35ec
Vladimir Patrikeev <vpatrikeev@likebz.ru>
2022-06-12 22:54:20 +0300

    Fix tonality vectorizer

--- storage/ml/tonality/main.py
+++ storage/ml/tonality/main.py
@@ -6,7 +6,7 @@ parser.add_argument('input_csv', type=str, help='Input csv path')
 parser.add_argument('output_csv', type=str, help='Output csv path')
 args = parser.parse_args()
 
-modelS = predict_sentiments('/var/www/html/storage/ml/tonality/NN','/var/www/html/storage/ml/reliability/vectorizer_sentiments.pkl')
+modelS = predict_sentiments('/var/www/html/storage/ml/tonality/NN','/var/www/html/storage/ml/tonality/vectorizer_sentiments.pkl')
 modelS.load_and_clean_data(args.input_csv)
 
 result = modelS.predicted_outputs()
